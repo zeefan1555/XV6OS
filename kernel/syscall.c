2d6c910ed2392a2a4e68548ae0b864b320cab4b2
zeefan <1145468360@qq.com>
2023-08-23 15:24:29 +0800

    trace done

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -127,6 +127,33 @@ static uint64 (*syscalls[])(void) = {
 [SYS_link]    sys_link,
 [SYS_mkdir]   sys_mkdir,
 [SYS_close]   sys_close,
+[SYS_trace]   sys_trace,
+};
+
+
+static char *syscalls_name[] = {
+[SYS_fork]    "fork",
+[SYS_exit]    "exit",
+[SYS_wait]    "wait",
+[SYS_pipe]    "pipe",
+[SYS_read]    "read",
+[SYS_kill]    "kill",
+[SYS_exec]    "exec",
+[SYS_fstat]   "fstat",
+[SYS_chdir]   "chdir",
+[SYS_dup]     "dup",
+[SYS_getpid]  "getpid",
+[SYS_sbrk]    "sbrk",
+[SYS_sleep]   "sleep",
+[SYS_uptime]  "uptime",
+[SYS_open]    "open",
+[SYS_write]   "write",
+[SYS_mknod]   "mknod",
+[SYS_unlink]  "unlink",
+[SYS_link]    "link",
+[SYS_mkdir]   "mkdir",
+[SYS_close]   "close",
+[SYS_trace]   "trace",
 };
 
 void
@@ -135,9 +162,13 @@ syscall(void)
   int num;
   struct proc *p = myproc();
 
-  num = p->trapframe->a7;
+  num = p->trapframe->a7;// 系统调用编号，参见书中4.3节
   if(num > 0 && num < NELEM(syscalls) && syscalls[num]) {
     p->trapframe->a0 = syscalls[num]();
+
+    // 系统调用是否匹配
+    if((1 << num) & p->trace_mask)
+      printf("%d syscall %s -> %d\n", p->pid, syscalls_name[num], p->trapframe->a0 ) ;
   } else {
     printf("%d %s: unknown sys call %d\n",
             p->pid, p->name, num);
--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -95,3 +95,11 @@ sys_uptime(void)
   release(&tickslock);
   return xticks;
 }
+uint64
+sys_trace(void)
+{
+  // 获取系统调用的参数
+
+  argint(0, &(myproc()->trace_mask))
+  return 0;
+}
