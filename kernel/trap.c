079ffebcf39c5d4292b729c1cf415681835db960
zeefan <1145468360@qq.com>
2023-09-04 22:56:51 +0800

    fix (4). 修改usertrap，处理页面错误

--- kernel/trap.c
+++ kernel/trap.c
@@ -75,7 +75,6 @@ usertrap(void)
     p->killed = 1;
   }
     
-  }
   else {
     printf("usertrap(): unexpected scause %p pid=%d\n", r_scause(), p->pid);
     printf("            sepc=%p stval=%p\n", r_sepc(), r_stval());
